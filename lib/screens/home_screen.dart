b0caab72c71e9fee3bffd79f0868c944cf94235d
jodeyy <jodiptra@gmail.com>
2024-11-02 09:22:07 +0700

    commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: Title(title: "City Tour"),
+      body: SingleChildScrollView(
+        child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              children: [
+                SizedBox(height: 16,),
+
+                //Galeri Gambar
+                Row(
+                  children: [SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(18),
+                     
+
+                    ),
+                  )],
+                )
+              ],
+
+    ),
+
+    ),
+
+
+
+    ),
+    );
+  }
+}
